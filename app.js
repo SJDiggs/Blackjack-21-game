4cf84dacb90b77264bc31baec229db4d70834aa7
stevediggs70 <stevediggs670@gmail.com>
2023-10-30 11:28:42 -0400

    dealer hidden card logic and card value assignments

--- app.js
+++ app.js
@@ -6,17 +6,17 @@ console.log("JS Loaded")
 // ----- Variables ----- //
 
 
-// keep card totals for the player and dealer
-let playerTotal
-let dealerTotal
-// keep the account balance total
-let accountBalance
+// Card numerical values for the player and dealer
+let playerTotal = 0
+let dealerTotal = 0
+// Account balance total, initialized at $500
+let accountBalance = 500
 //init value to allow the player to draw a card if the value of playerTotal is less than 21
 let hitMe = true
 // if a player or dealer has an ace it could be evaluated as either a 1 or a 10 depending on the hand
 let playerAces
 let dealerAces
-// track value of dealers hidden card
+// Working storage to track the value of the dealers hidden card
 let hiddenCard
 let deck
 
@@ -44,7 +44,9 @@ function init() {
     shuffleCards();
     console.log("Cards shuffled")
     firstHand();
-
+    console.log("First Hand Delt")
+    console.log(hidden)
+    console.log(dealerTotal)
 
 }
 // This function will create the deck by using nested for loops to create the card value and suit (to be associated with the corresponding .png)  NOTE:  ForEach can be used here - icebox
@@ -71,7 +73,30 @@ function shuffleCards() {
 }
 
 function firstHand() {
-    
+    hidden = deck.pop() //grab the last card from the deck and assign it to the dealer hidden card
+    dealerTotal += getCardValue(hidden); //call getCardValue with the hidden card value
+    dealerAces += aceCheck(hidden); //check to see if the dealer has an ace as their hidden card
+
+}
+// This function will determine the value of a card if the card is a non-numerical card (i.e. Ace, King, Queen, Jack) and return the corresponding value of the cards
+function getCardValue(card) {
+    let data = card.split("-") // isolate the card value and suit (2-D --> ["2", "D"])  
+    let cardValue = data[0]
+    if (cardValue == "J" || cardValue == "Q" || cardValue == "K") { //convert face-cards to number 10
+        return 10
+    }
+    if (cardValue = "A") { //convert ace to 11
+        return 11
+    }
+    return parseInt(cardValue) //return the numerical value of the card for all non-face-cards or aces by parsing the deck string and returning the corresponding integer
+}
+
+function aceCheck(card) {
+    if (card[0] == "A") {
+        return 1
+    } else {
+        return 0
+    }
 }
 // render
 // winning
