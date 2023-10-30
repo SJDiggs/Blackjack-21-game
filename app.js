bcebd77a229583dfc73c9dfcf1cc4ca4951782f9
stevediggs70 <stevediggs670@gmail.com>
2023-10-29 23:07:21 -0400

    Added images for action buttons and wagers

--- app.js
+++ app.js
@@ -1,1 +1,27 @@
 console.log("JS Loaded")
+// ----- Constants ----- //
+
+
+
+// ----- Variables ----- //
+
+
+
+// ----- Cached DOM elements ----- //
+
+
+
+// ----- Event Listeners ----- //
+
+
+
+// ----- Functions ----- //
+// init
+// render
+// random card calculator
+// winning
+// message system
+// account balance add/decrease
+// leave game
+// deal hand
+// hit logic
